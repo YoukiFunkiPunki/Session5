c529e2f593e2c9d2b0f4c0ee211f092d7a782970
Thomas <126421339+YoukiFunkiPunki@users.noreply.github.com>
2023-03-14 09:48:08 +0100

    Update Bike.h

--- session 5/Bike.h
+++ session 5/Bike.h
@@ -1,8 +1,24 @@
-//
-// Created by thoma on 14/03/2023.
-//
-
+#include <iostream>
+#include "vehicle.h"
 #ifndef UNTITLED_BIKE_H
 #define UNTITLED_BIKE_H
+using namespace std;
+
+class bike:public vehicle {
+protected:
+    double m_speed;
+public:
+    bike(double,double);
+    void afficher();
+    double get_price();
+    int get_type();
+};
+
+bike::bike(double a, double b):vehicle(a){m_speed=b;}
+double bike::get_price(){return(m_price,m_speed);}
+void bike::afficher(){cout<<"Price : "<<m_price<<" euro for "<<m_speed<<" km/h max speed, type "<<get_type()<<endl;}
+int bike::get_type(){return 2;}
+
+
 
 #endif //UNTITLED_BIKE_H
