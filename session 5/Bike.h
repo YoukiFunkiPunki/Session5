961cbacec8eba7321f7a49a9fae933c088f23d09
Thomas <126421339+YoukiFunkiPunki@users.noreply.github.com>
2023-03-14 11:51:22 +0100

    Update Bike.h

--- session 5/Bike.h
+++ session 5/Bike.h
@@ -10,13 +10,15 @@ protected:
 public:
     bike(double,double);
     virtual void afficher();
-    double get_price();
-    int get_type();
+    virtual double get_price();
+    virtual int get_type();
+    virtual ~bike(){};
 };
 
 bike::bike(double a, double b):vehicle(a){m_speed=b;}
 double bike::get_price(){return(m_price,m_speed);}
-void bike::afficher(){cout<<"Price : "<<m_price<<" euro for "<<m_speed<<" km/h max speed, type "<<get_type()<<endl;}
+void bike::afficher(){cout<<"This is a bike"<<endl;
+    cout<<"Price : "<<m_price<<" euro for "<<m_speed<<" km/h max speed, type "<<get_type()<<endl;}
 int bike::get_type(){return 2;}
 
 
