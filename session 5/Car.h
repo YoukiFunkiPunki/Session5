7d12e3e1d1b2c91325cb82b90fdfda6f0bb277a6
Thomas <126421339+YoukiFunkiPunki@users.noreply.github.com>
2023-03-14 11:50:37 +0100

    Update Car.h

--- session 5/Car.h
+++ session 5/Car.h
@@ -10,13 +10,15 @@ protected:
 public:
     car(double,int);
     virtual void afficher();
-    double get_price();
-    int get_type();
+    virtual double get_price();
+    virtual int get_type();
+    virtual ~car(){};
 };
 
 car::car(double a, int n):vehicle(a),m_doors(n) {}
 double car::get_price(){return(m_price,m_doors);}
-void car::afficher(){cout<<"Price : "<<m_price<<" euro for "<<m_doors<<" doors, type "<<get_type()<<endl;}
+void car::afficher(){cout<<"This is a car"<<endl;
+    cout<<"Price : "<<m_price<<" euro for "<<m_doors<<" doors, type "<<get_type()<<endl;}
 int car::get_type(){return 1;}
 
 #endif //UNTITLED_CAR_H
